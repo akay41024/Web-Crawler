0c34944d8802cbc58839bab2172ea92e84bfe516
Aasif khan <akay41024@gmail.com>
2025-02-01 00:34:14 +0530

    remove new1.py file containing version retrieval methods for crawl4ai

--- new1.py
+++
@@ -1,8 +0,0 @@
-import crawl4ai
-
-# Method 1: Using __version__ attribute (if available)
-print(crawl4ai.__version__)  # Direct version string access
-
-# Method 2: Using importlib.metadata (more reliable)
-from importlib.metadata import version
-print(version('crawl4ai'))
